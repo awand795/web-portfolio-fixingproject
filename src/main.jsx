3ca84c893b505804d0398959b2d21d72ce0fdafe
awand795 <awand795@gmail.com>
2026-06-10 15:31:23 +0700

    fix: resolve broken layout by importing CSS and fixing Netlify Node version

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './router'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { LanguageProvider } from './context/LanguageContext'
 import { ThemeProvider } from './context/ThemeContext'
